095fe627dd15562d826d1eefdfdec5240688afa2
Sherief Elnahas <sheriefelnahas5@gmail.com>
2022-11-15 14:25:13 +0200

    Start Quiz Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import './App.css';
+import {useState } from 'react';
+import StartQuiz from './StartQuiz';
+
 function App() {
+  const [started,setstarted] = useState(false);
+
+  function start() {
+    setstarted(true);
+  }
+
   return (
     <div className="App">
-      <h1>App</h1>
+      {!started &&       <StartQuiz start={start}/>}
+
     </div>
   )
 }
