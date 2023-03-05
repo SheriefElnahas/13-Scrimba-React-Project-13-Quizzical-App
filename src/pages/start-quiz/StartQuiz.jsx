41278e652feb870ef8c412300511d1d97585d31e
Sherief Elnahas <sheriefelnahas5@gmail.com>
2023-03-05 07:46:42 +0200

    StartQuiz Component HTML & CSS

--- src/pages/start-quiz/StartQuiz.jsx
+++ src/pages/start-quiz/StartQuiz.jsx
@@ -11,48 +11,54 @@ export default function StartQuiz() {
 
       <form>
         <h2>Choose how you want to test yourself.</h2>
-        <p>
-          <label htmlFor="category">Select The Category</label>
-          <select id="category">
-            <option value="" disabled>
+        <div className={styles['user-choices']}>
+          <p>
+            <label htmlFor="category">Select The Category</label>
+            <select id="category">
+              <option value="" disabled selected>
+                -- Choose A Category --
+              </option>
+              <option value="attack-on-titan">General Knowledge</option>
+              <option value="vinland-saga">Science : Nature</option>
+              <option value="vinland-saga">Science : Computers</option>
+              <option value="vinland-saga">Science : Mathematics</option>
+              <option value="vinland-saga">Sports</option>
+              <option value="vinland-saga">Geography</option>
+              <option value="vinland-saga">History</option>
+            </select>
+          </p>
+          <p>
+            <label>
               {' '}
-              -- Choose --{' '}
-            </option>
-            <option value="attack-on-titan">Attack On Titan</option>
-            <option value="vinland-saga">Vinland Saga</option>
-            <option value="demon-slayer">Demon Slayer</option>
-          </select>
-        </p>
+              Write the number of questions
+              <input type="number" placeholder="Questions number between 1 : 50" />
+            </label>
+          </p>
+          <p>
+            <label htmlFor="category">Select The Type</label>
+            <select id="questions-type">
+              <option value="" disabled selected>
+                -- Choose A Type --
+              </option>
+              <option value="attack-on-titan">Attack On Titan</option>
+              <option value="vinland-saga">Vinland Saga</option>
+              <option value="demon-slayer">Demon Slayer</option>
+            </select>
+          </p>
+          <p>
+            <label htmlFor="difficulty">Select The Quiz Difficulty</label>
+            <select id="questions-type">
+              <option value="" disabled selected>
+                -- Choose A Level --
+              </option>
+              <option value="attack-on-titan">Attack On Titan</option>
+              <option value="vinland-saga">Vinland Saga</option>
+              <option value="demon-slayer">Demon Slayer</option>
+            </select>
+          </p>
+        </div>
         <p>
-          <label>
-            {' '}
-            Write the number of questions
-            <input type="number" />
-          </label>
-        </p>
-        <p>
-          <label htmlFor="category">Select The Category</label>
-          <select id="questions-type">
-            <option value="" disabled>
-              {' '}
-              -- Choose --{' '}
-            </option>
-            <option value="attack-on-titan">Attack On Titan</option>
-            <option value="vinland-saga">Vinland Saga</option>
-            <option value="demon-slayer">Demon Slayer</option>
-          </select>
-        </p>
-        <p>
-          <label htmlFor="difficulty">Choose The Quiz Difficulty</label>
-          <select id="questions-type">
-            <option value="" disabled>
-              {' '}
-              -- Choose --{' '}
-            </option>
-            <option value="attack-on-titan">Attack On Titan</option>
-            <option value="vinland-saga">Vinland Saga</option>
-            <option value="demon-slayer">Demon Slayer</option>
-          </select>
+          <button>Start Quizz</button>
         </p>
       </form>
     </section>
